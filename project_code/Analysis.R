0eb0d0546564a2630ab3347e266f759a611d1531
danjwalton <20973540+danjwalton@users.noreply.github.com>
2020-03-25 13:17:25 +0000

    ID-relevant aid analysis

---
+++ project_code/Analysis.R
@@ -0,0 +1,113 @@
+#danjwalton 2019
+required.packages <- c("data.table", "RJSONIO", "WDI", "readxl")
+lapply(required.packages, require, character.only = T)
+
+wd <- "G:/My Drive/Work/GitHub/disability_aid_analysis/"
+setwd(wd)
+
+source("https://raw.githubusercontent.com/danjwalton/crs_keyword_searching/master/project_code/load_and_join.R")
+crs <- load_crs(path="output")
+dpos <- read.csv("project_data/dpos.csv", header=F, encoding = 'UTF-8')
+pop <- as.data.table(WDI("all","SP.POP.TOTL", start=2014, end=2018))
+
+pop[country=="China"]$country <- "China (People's Republic of)"
+pop[country=="Congo, Rep."]$country <- "Congo"
+pop[country=="Cote d'Ivoire"]$country <- "Côte d'Ivoire"
+pop[country=="Korea, Dem. Peopleâ€™s Rep."]$country <- "Democratic People's Republic of Korea"
+pop[country=="Congo, Dem. Rep."]$country <- "Democratic Republic of the Congo"
+pop[country=="Egypt, Arab Rep."]$country <- "Egypt"
+pop[country=="Kyrgyz Republic"]$country <- "Kyrgyzstan"
+pop[country=="Lao PDR"]$country <- "Lao People's Democratic Republic"
+pop[country=="Micronesia, Fed. Sts."]$country <- "Micronesia"
+pop[country=="Venezuela, RB"]$country <- "Venezuela"
+pop[country=="Vietnam"]$country <- "Viet Nam"
+pop[country=="West Bank and Gaza"]$country <- "West Bank and Gaza Strip"
+pop[country=="Yemen, Rep."]$country <- "Yemen"
+
+channel.codes <- read_excel("project_data/DAC-CRS-CODES.xls", sheet="Channel codes", skip=6)
+top.channel.codes <- channel.codes[!duplicated(channel.codes$`Channel Parent Category`),][,c("Channel ID", "Full Name (English)")]
+names(top.channel.codes) <- c("Channel ID", "ParentChannelName")
+
+ffwrite <- function(x, path="output/"){
+  if(!is.data.table(x))stop("Data is not a data.table object")
+  dataname <- deparse(substitute(x))
+  fwrite(x, paste0(path, dataname, ".csv"))
+}
+
+#P/S TOTAL DISABILITY
+split.years <- dcast(crs, Year ~ relevance, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+split.years[, (paste0(names(split.years)[names(split.years) != "Year"], ".share")) := .SD/sum(.SD), .SDcols = (names(split.years)[names(split.years) != "Year"]), by=Year]
+ffwrite(split.years)
+
+#ID
+total.id.years <- crs[, .(ID = sum(USD_Disbursement_Defl[relevance != "None" & intellectual == "intellectual"], na.rm=T), Other = sum(USD_Disbursement_Defl[relevance != "None" & intellectual != "intellectual"],na.rm=T), None = sum(USD_Disbursement_Defl[relevance == "None"], na.rm=T)), by=Year]
+total.id.years[, (paste0(names(total.id.years)[names(total.id.years) != "Year"], ".share")) := .SD/sum(.SD), .SDcols = (names(total.id.years)[names(total.id.years) != "Year"]), by=Year]
+total.id.years <- total.id.years[order(Year)]
+ffwrite(total.id.years)
+
+#P/S SPLIT ID
+split.id.years <- dcast(crs[intellectual == "intellectual"], Year ~ relevance + intellectual, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+split.id.years[, (paste0(names(split.id.years)[names(split.id.years) != "Year"], ".share")) := .SD/sum(.SD), .SDcols = (names(split.id.years)[names(split.id.years) != "Year"]), by=Year]
+ffwrite(split.id.years)
+
+#SECTORS ID
+sectors.id.years <- dcast(crs[intellectual == "intellectual"], SectorName + relevance ~ Year, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+sectors.id.years[, c("SectorID", "SectorName") := tstrsplit(sectors.id.years$SectorName, "[.] ")]
+sectors.id.years[is.na(SectorName)]$SectorName <- sectors.id.years[is.na(SectorName)]$SectorID
+sectors.id.years$SectorTopLevel <- sectors.id.years$SectorName
+sectors.id.years[grepl("^I[.]1[.]", SectorID)]$SectorTopLevel <- "Education"
+sectors.id.years[grepl("^I[.]2[.]", SectorID)]$SectorTopLevel <- "Health"
+sectors.id.years[grepl("^I[.]3[.]", SectorID)]$SectorTopLevel <- "Population Policies/Programmes & Reproductive Health"
+sectors.id.years[grepl("^I[.]4[.]", SectorID)]$SectorTopLevel <- "Water Supply & Sanitation"
+sectors.id.years[grepl("^I[.]5[.]", SectorID)]$SectorTopLevel <- "Government & Civil Society"
+sectors.id.years[grepl("^I[.]6[.]", SectorID)]$SectorTopLevel <- "Other Social Infrastructure & Services"
+ffwrite(sectors.id.years)
+
+#DONORS ID
+donors.id.years <- dcast(crs, Year + DonorName ~ intellectual, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+donors.id.years[, (paste0(names(donors.id.years)[!(names(donors.id.years) %in% c("Year", "DonorName"))], ".share")) := .SD/sum(.SD), .SDcols = (names(donors.id.years)[!(names(donors.id.years) %in% c("Year", "DonorName"))]), by=Year]
+ffwrite(donors.id.years)
+
+#RECIPIENTS ID
+recipients.id.years <- dcast(crs[intellectual == "intellectual"], Year + RecipientName ~ ., value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+recipients.id.years <- merge(recipients.id.years, pop[,c("country", "SP.POP.TOTL", "year")], by.x=c("Year", "RecipientName"), by.y=c("year", "country"))
+recipients.id.years <- recipients.id.years[, .(intellectual = `.`, per.capita = `.`/SP.POP.TOTL), by=.(Year, RecipientName)]
+ffwrite(recipients.id.years)
+
+#ID SUBPURPOSE
+crs$intellectual.subpurpose <- "Other"
+crs[(employment == "employment" | education == "education" | family == "family")]$intellectual.subpurpose <- "Mixed"
+crs[employment == "employment" & education != "education" & family != "family"]$intellectual.subpurpose <- "Employment"
+crs[employment != "employment" & education == "education" & family != "family"]$intellectual.subpurpose <- "Education"
+crs[employment != "employment" & education != "education" & family == "family"]$intellectual.subpurpose <- "Family"
+subpurpose.id.years <- dcast(crs[relevance != "None" | intellectual == "intellectual"], intellectual + intellectual.subpurpose ~ Year, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+ffwrite(subpurpose.id.years)
+
+#ID GENDER
+gender.id.years <- dcast(crs[intellectual == "intellectual"], Year ~ Gender, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+gender.id.years[, (paste0(names(gender.id.years)[names(gender.id.years) != "Year"], ".share")) := .SD/sum(.SD), .SDcols = (names(gender.id.years)[names(gender.id.years) != "Year"]), by=Year]
+ffwrite(gender.id.years)
+
+#CHANNELS ID
+firstCap <- function(y) {
+  sapply(y, function(x) {
+    x <- as.character(x)
+    s <- strsplit(x, " ")[[1]]
+    paste(sapply(s, function(t) {
+      ifelse(toupper(t) == t, t, paste0(toupper(substring(tolower(t), 1,1)), substring(tolower(t), 2)))
+    }
+    ), collapse=" ")
+  }
+  )
+}
+crs$channel <- firstCap(crs$ChannelReportedName)
+crs$dpo <- "Other channel"
+crs[channel %in% dpos$V1]$dpo <- "DPO"
+crs$ParentChannelCode <- floor(crs$ParentChannelCode/1000)*1000
+crs$TopChannelCode <- floor(crs$ParentChannelCode/10000)*10000
+crs <- merge(crs, top.channel.codes, by.x="ParentChannelCode", by.y="Channel ID")
+
+dpos.id.years <- dcast(crs[intellectual == "intellectual"], dpo + ParentChannelName ~ Year, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+channels.id.years <- dcast(crs[intellectual == "intellectual"], channel + ChannelName ~ Year, value.var = "USD_Disbursement_Defl", fun.aggregate = function (x) sum(x, na.rm=T))
+
+
---
+++ project_code/Keyword searches.R
@@ -0,0 +1,362 @@
+#danjwalton 2019
+
+required.packages <- c("data.table", "RJSONIO")
+lapply(required.packages, require, character.only = T)
+
+wd <- "G:/My Drive/Work/GitHub/disability_aid_analysis/"
+setwd(wd)
+
+source("https://raw.githubusercontent.com/danjwalton/crs_keyword_searching/master/project_code/load_and_join.R")
+crs <- load_crs()
+
+keep <- c(
+  "CrsID"
+  ,
+  "ProjectNumber"
+  ,
+  "Year"
+  ,
+  "Aid_t"
+  ,
+  "FlowName"
+  ,
+  "DonorName"
+  ,
+  "RecipientName"
+  ,
+  "USD_Commitment_Defl"
+  ,
+  "USD_Disbursement_Defl"
+  ,
+  "SectorName"
+  ,
+  "PurposeName"
+  ,
+  "ProjectTitle"
+  ,
+  "ShortDescription"
+  ,
+  "LongDescription"
+  ,
+  "Gender"
+  ,
+  "Disability"
+  ,
+  "ChannelReportedName"
+  ,
+  "ChannelName"
+  ,
+  "ChannelCode"
+  ,
+  "ParentChannelCode"
+)
+
+crs <- crs[, ..keep]
+crs <- crs[
+    FlowName == "ODA Loans" 
+  |
+    FlowName == "ODA Grants"
+  | 
+    FlowName == "Equity Investment"
+  | 
+    FlowName == "Private Development Finance"
+  ]
+
+crs <- crs[as.character(Year) >= 2014]
+
+major.keywords <- c(
+  "disab", "discapaci", "incapaci", "minusválido", "invalidit", "infirmité", "d-isab"
+  #,
+  #"disorder"
+  ,
+  "handicap"
+  ,
+  "impairment", "impaired"
+  ,
+  "pwd", "gwd", "cwd"
+  ,
+  "chronic health", "chronic ill", "maladie chronique", "enfermedad crónica"
+  ,
+  "deaf", "sordo", "sourd"
+  ,
+  "blind", "ciego", "aveugle", "eye health"
+  ,
+  "special needs", "necesidades especiales", "besoins spéciau"
+  ,
+  "autistic", "autism", "autist"
+  ,
+  "mental health", "santé mentale", "salud mental"
+  ,
+  "prosthe", "prosthè", "prótesi"
+  ,
+  "mobility device", "dispositivo de movilidad", "dispositif de mobilité"
+  ,
+  "wheelchair", "fauteuil roulant", "silla de ruedas"
+  ,
+  "hearing aid", "audífono", "dispositif d'écoute pour malentendant"
+  ,
+  "amputation", "amputee", "amputé", "amputa"
+  ,
+  "schizophreni", "esquizofrenia", "schizophrénie"
+  ,
+  "sign language", "langage des signes", "lenguaje de señas"
+  ,
+  "arthriti", "artritis", "arthrite"
+  ,
+  "rheumat", "rhumat", "reumat"
+  ,
+  "dementia", "démence", "demencia"
+  ,
+  "spina "
+  ,
+  "hydrocephalus", "hidrocefalia", "l'hydrocéphalie"
+  ,
+  "diabetes", "diabète"
+  ,
+  "atlas alliance", "atlas allinance", "abilis foundation", "zapdd"
+  #,
+  #"dpos ", "dpo ", "dpo's", "dpos[.]", "dpo[.]", "dpo's[.]"
+  ,
+  "special education", "educación especial", "éducation spéciale"
+  ,
+  "learning difficult", "learning disa", "difficultés d'apprentissage", "dificultades de aprendizaje", "discapacidad de aprendizaje", "trouble d'apprentissage", "learning problem"
+  ,
+  "trisomy.{0,1}21", "trisomie.{0,1}21", "trisomía.{0,1}21"
+  ,
+  "down syndrom", "syndrome de down", "síndrome de down"
+  ,
+  "cerebral",	"cérébrale"
+  ,
+  "crpd"
+  ,
+  "psycho.{0,1}social"
+)
+
+
+minor.keywords <- c(
+  "vulnerable group", "vulnerable people", "vulnerable population", "vulnerable individual", "vulnerable girl", "vulnerable women", "vulnerable boy", "vulnerable men", "vulnerable refugee", "who are vulnerable", "which are vulnerable", "vulnerable child"
+  ,
+  #"minority group", "minority population", "minorities"
+  #,
+  "marginali.ed group", "marginali.ed people", "marginali.ed population", "marginali.ed individual", "marginali.ed girl", "marginali.ed women", "marginali.ed boy", "marginali.ed men", "marginali.ed refugee", "who are marginali.ed", "which are marginali.ed", "marginali.ed child", "marginali.ed and young"
+  ,
+  "war victim", "victimas de guerra", "victimes de guerre", "victim. of war"
+  ,
+  #"conflict affected", "conflict-affected", "conflict victim", "victim. of conflict"
+  #,
+  "landmine victim", "victime de mine", "víctima de minas terrestres"
+  #,
+  #"wounded"
+  #,
+  #"injured", "injuries"
+  #,
+  #"physiotherapy", "fisioterapia"
+)
+
+disqualifying.keywords <- c(
+  "ciego de avila"
+  ,
+  "chronic malnutrition"
+  ,
+  "mole rat"
+  ,
+  "cgpwd"
+  ,
+  "cpwd"
+  ,
+  "rvcwda"
+  ,
+  "pwdtc"
+  ,
+  " road"
+  ,
+  "highway"
+  ,
+  "environmental health"
+  ,
+  "environmental condition"
+  ,
+  "rehydration therapy"
+  #,
+  #"-dpo", "cidpo", "hdpo", "dpo series", "financial sector dpo", "dpo (ri)", "management dpo", "poverty dpo", "growth dpo", "support dpo", "system dpo", "programmatic dpo"
+  ,
+  "fiscal"
+  ,
+  "growth and compet"
+  ,
+  "combination therap"
+  ,
+  "emergency dpo"
+  ,
+  "conventional weapons", "weapons destruction"
+  ,
+  "fairtradeafrica"
+  ,
+  "blindness prevention", "avoidable blindness"
+)
+
+disqualifying.sectors <- c(
+  "Public finance management (PFM)"
+  ,
+  "Domestic revenue mobilisation"
+  #,
+  #"Mineral/mining policy and administrative management"
+)
+
+channel.keywords <- c(
+  "abilis foundation"
+  ,
+  "disab"
+  ,
+  "atlas alliance", "atlas-alliansen"
+  ,
+  "handicap international"
+)
+
+inclusion.keywords <- c(
+  "inclus"
+  ,
+  "empower", "habiliter", "autorizar"
+  ,
+  "rights", "droits", "derechos"
+  ,
+  "advocacy", "plaidoyer", "abogacía"
+  ,
+  "self-representative", "auto-représentant",	"auto-representante"
+  ,
+  "autonomy", "autonomie", "autonomía"
+  ,
+  #"community", "communaté"
+  "integration", "intégration", "integración"
+)
+
+employment.keywords <- c(
+  "employ", "emplear", "empleo", "emploi"
+  ,
+  "travail", "trabajo"
+  ,
+  "job"
+  ,
+  "labour", "labor[.]", "labor "
+  ,
+  "cash for work"
+  ,
+  "vocation", "vocación"
+  ,
+  "profession", "profesión"
+  ,
+  "skills", "compétences",	"habilidades"
+  ,
+  "livelihood", "moyens de subsistance", "sustento"
+  ,
+  "earning", "revenus",	"ganador"
+  ,
+  "microcredit", "microcrédit"
+  ,
+  "article.{0,1}27"
+  ,
+  "workshop", "atelier"
+  ,
+  "business", "affaires",	"negocio"
+  ,
+  "workplace"
+  ,
+  "social protection", "social security", "sécurité sociale", "protection sociale",	"protección social", "seguridad social"
+)
+
+intellectual.keywords <- c(
+  "intellect", "intelect"
+  ,
+  "cognitive", "cognitiva"
+  ,
+  "autistic", "austism", "autist"
+  ,
+  "special needs", "necesidades especiales", "besoins spéciau", "besoins spécifiques"
+  ,
+  "special education", "educación especial", "éducation spéciale"
+  ,
+  "learning diff", "learning disa", "difficultés d'apprentissage", "dificultades de aprendizaje", "discapacidad de aprendizaje", "trouble d'apprentissage"
+  ,
+  "developmental disab", "developmental disorder", "development disab", "development disorder", "trouble du développement", "discapacidad de desarrollo", "discapacidades del desarrollo", "trastorno del desarrollo"
+  ,
+  "trisomy.{0,1}21", "trisomie.{0,1}21", "trisomía.{0,1}21"
+  ,
+  "down syndrom", "syndrome de down", "síndrome de down"
+  ,
+  "complex support", "soutien complexe", "apoyo compleja"
+  ,
+  "cerebral",	"cérébrale"
+  ,
+  "psycho.{0,1}social"
+)
+
+education.keywords <- c(
+  "educat", "éducation", "educa", "éduquer"
+  ,
+  "learning", "apprentissage", "aprendizaje"
+  ,
+  "article.{0,1}24"
+  ,
+  "class", "classe",	"clase"
+  ,
+  #"study", "estudiar", "étude"
+  #,
+  "school", "college", "university", "escula", "colegio", "universidad", "école", "collège", "université"
+  ,
+  "teach", "enseñar", "enseigner"
+)
+
+family.keywords <- c(
+  "parent", "padre"
+  ,
+  "sibling", "fratrie", "hermano"
+  ,
+  "family", "families", "famille", "familia"
+  ,
+  "article.{0,1}23"
+)
+
+
+crs$Gender <- as.character(crs$Gender)
+crs[is.na(Gender)]$Gender <- "Not screened"
+crs[Gender == "0"]$Gender <- "No gender component"
+crs[Gender == "1"]$Gender <- "Significant gender component"
+crs[Gender == "2"]$Gender <- "Principal gender component"
+
+crs$Disability <- as.character(crs$Disability)
+crs[is.na(Disability)]$Disability <- "Not screened"
+crs[Disability == "0"]$Disability <- "No disability component"
+crs[Disability == "1"]$Disability <- "Significant disability component"
+crs[Disability == "2"]$Disability <- "Principal disability component"
+
+crs$relevance <- "None"
+crs[grepl(paste(minor.keywords, collapse = "|"), tolower(paste(crs$ProjectTitle, crs$ShortDescription, crs$LongDescription)))]$relevance <- "Minor"
+crs[grepl(paste(major.keywords, collapse = "|"), tolower(crs$LongDescription))]$relevance <- "Minor"
+crs[grepl(paste(major.keywords, collapse = "|"), tolower(paste(crs$ShortDescription, crs$ProjectTitle)))]$relevance <- "Major"
+crs[grepl(paste(channel.keywords, collapse = "|"), tolower(crs$ChannelReportedName))]$relevance <- "Major"
+
+crs$check <- "No"
+crs[relevance == "Minor"]$check <- "potential false positive"
+crs[relevance != "None"][PurposeName %in% disqualifying.sectors]$check <- "potential false negative"
+crs[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$check <- "potential false negative"
+
+crs[relevance != "None"][grepl(paste(disqualifying.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$relevance <- "None"
+crs[relevance != "None"][PurposeName %in% disqualifying.sectors]$relevance <- "None"
+
+crs$inclusion <- "Not inclusion"
+crs[relevance != "None"][grepl(paste(inclusion.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$inclusion <- "inclusion"
+
+crs$employment <- "Not employment"
+crs[relevance != "None"][grepl(paste(employment.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$employment <- "employment"
+
+crs$intellectual <- "Not intellectual"
+crs[relevance != "None" | Disability %in% c("Principal disability component", "Significant disability component")][grepl(paste(intellectual.keywords, collapse = "|"), tolower(paste(crs[relevance != "None" | Disability %in% c("Principal disability component", "Significant disability component")]$ProjectTitle, crs[relevance != "None" | Disability %in% c("Principal disability component", "Significant disability component")]$ShortDescription, crs[relevance != "None" | Disability %in% c("Principal disability component", "Significant disability component")]$LongDescription)))]$intellectual <- "intellectual"
+
+crs$education <- "Not education"
+crs[relevance != "None"][grepl(paste(education.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$education <- "education"
+
+crs$family <- "Not family"
+crs[relevance != "None"][grepl(paste(family.keywords, collapse = "|"), tolower(paste(crs[relevance != "None"]$ProjectTitle, crs[relevance != "None"]$ShortDescription, crs[relevance != "None"]$LongDescription)))]$family <- "family"
+
+source("https://raw.githubusercontent.com/danjwalton/crs_keyword_searching/master/project_code/split_and_save.R")
+split_and_save(crs, "output", 0)
